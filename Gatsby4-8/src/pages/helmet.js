80d6e4c6352e9b19c4fe1d8e3e68ce5c36b737a4
rcalder7 <46700832+rcalder7@users.noreply.github.com>
2019-02-23 02:42:55 -0600

    Add files via upload

---
+++ Gatsby4-8/src/pages/helmet.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { Helmet } from "react-helmet"
+
+class Application extends React.Component {
+  render() {
+    return (
+      <div className="application">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>Brandon Mendez</title>
+          <link rel="canonical" href="http://mysite.com/example" />
+        </Helmet>
+        ...
+      </div>
+    )
+  }
+}
